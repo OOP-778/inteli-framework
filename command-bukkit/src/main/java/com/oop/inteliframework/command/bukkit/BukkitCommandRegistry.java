154f49efc4b7843d2b7db82ccac6167a82f0250e
oop-778 <oskardhavel@gmail.com>
2021-03-13 00:54:48 +0200

    Made so commands are unregistered on disable

--- command-bukkit/src/main/java/com/oop/inteliframework/command/bukkit/BukkitCommandRegistry.java
+++ command-bukkit/src/main/java/com/oop/inteliframework/command/bukkit/BukkitCommandRegistry.java
@@ -2,12 +2,16 @@ package com.oop.inteliframework.command.bukkit;
 
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.oop.inteliframework.command.element.command.Command;
 import com.oop.inteliframework.command.registry.CommandRegistry;
+import com.oop.inteliframework.commons.util.InteliPair;
 import com.oop.inteliframework.commons.util.InteliVersion;
 import com.oop.inteliframework.commons.util.SimpleReflection;
 import com.oop.inteliframework.packetinjector.Injector;
 import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,6 +21,7 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.server.PluginDisableEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.lang.reflect.Constructor;
 import java.util.List;
 
 public class BukkitCommandRegistry extends CommandRegistry implements Listener {
@@ -48,15 +53,28 @@ public class BukkitCommandRegistry extends CommandRegistry implements Listener {
     injector.onFiltered(
         PACKET_PLAY_IN_TAB_COMPLETE,
         (player, packet) -> {
-          // Current Input
-          String s = extractContent(packet);
 
-          List<String> strings = tabComplete(new BukkitCommandExecutor(player), s);
+          // Current Input
+          String content = extractContent(packet);
+          if (!content.startsWith("/")) return true;
+
+          List<String> strings = tabComplete(new BukkitCommandExecutor(player), content);
           if (strings.isEmpty()) return true;
 
-          Injector.PlayerConnectionHelper.sendPacket(player, constructSuggestions(s, strings));
+          Injector.PlayerConnectionHelper.sendPacket(player, constructSuggestions(content, strings));
           return false;
         });
+
+    // If plugin has been reloaded, hook into players rn
+    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+      injector.inject(onlinePlayer);
+    }
+  }
+
+  @Override
+  public void register(Command command) {
+    super.register(command);
+    commandMap.register(command);
   }
 
   @EventHandler(priority = EventPriority.LOWEST)
@@ -80,23 +98,28 @@ public class BukkitCommandRegistry extends CommandRegistry implements Listener {
   @EventHandler
   public void onDisable(PluginDisableEvent event) {
     if (event.getPlugin().getName().equalsIgnoreCase(registerer.getName())) {
+      // Remove all packet listeners
       injector.unregisterAll();
+
+      // Remove all registered commands
+      commandMap.unregisterAll();
     }
   }
 
   /** We extract the current string from tab completion */
   @SneakyThrows
   private String extractContent(Object packet) {
-    return (String)
-        SimpleReflection.getField(PACKET_PLAY_IN_TAB_COMPLETE, String.class).get(packet);
+    return (String) SimpleReflection.getField(PACKET_PLAY_IN_TAB_COMPLETE, String.class).get(packet);
   }
 
   /** Construct suggestions packet */
   @SneakyThrows
   public Object constructSuggestions(String input, List<String> suggestions) {
-    if (InteliVersion.isBefore(13))
-      return SimpleReflection.getConstructor(PACKET_PLAY_OUT_TAB_COMPLETE, String[].class)
-          .newInstance(suggestions.toArray());
+    if (InteliVersion.isBefore(13)) {
+      return SimpleReflection
+              .getConstructor(PACKET_PLAY_OUT_TAB_COMPLETE, String[].class)
+              .newInstance((Object) suggestions.toArray(new String[0]));
+    }
 
     SuggestionsBuilder suggestionsBuilder = new SuggestionsBuilder(input, input.length());
     for (String suggestion : suggestions) suggestionsBuilder.suggest(suggestion);
