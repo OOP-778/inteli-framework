8a4c0a991074693183befee822f3d290ca62aa69
oop-778 <oskardhavel@gmail.com>
2021-03-26 21:09:36 +0200

    New universal animation system

---
+++ animation/src/main/java/com/oop/inteliframework/animation/InteliAnimationModule.java
@@ -0,0 +1,22 @@
+package com.oop.inteliframework.animation;
+
+import com.oop.inteliframework.animation.provider.AnimationProvider;
+import com.oop.inteliframework.animation.provider.EraseAnimation;
+import com.oop.inteliframework.plugin.module.InteliModule;
+import lombok.Getter;
+
+import java.util.TreeMap;
+
+@Getter
+public class InteliAnimationModule implements InteliModule {
+    private final TreeMap<String, AnimationProvider> providerTreeMap = new TreeMap<>(String::compareToIgnoreCase);
+
+    public InteliAnimationModule() {
+        registerProvider("erase", new EraseAnimation());
+    }
+
+    public void registerProvider(String name, AnimationProvider provider) {
+        providerTreeMap.put(name, provider);
+    }
+
+}
