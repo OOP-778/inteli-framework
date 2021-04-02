a50c6be8d13eeb842eeb0e40a34a02ea8c4f4c0c
oop-778 <oskardhavel@gmail.com>
2021-04-03 01:59:28 +0300

    Changed AbstractProvider cast of number to long

--- animation/src/main/java/com/oop/inteliframework/animation/provider/AbstractProvider.java
+++ animation/src/main/java/com/oop/inteliframework/animation/provider/AbstractProvider.java
@@ -3,14 +3,13 @@ package com.oop.inteliframework.animation.provider;
 import com.oop.inteliframework.animation.ContentAnimation;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public abstract class AbstractProvider implements AnimationProvider {
 
     @Override
     public ContentAnimation create(String text, Map<String, Object> options) {
         ContentAnimation animation = new ContentAnimation();
-        animation.interval((long) options.computeIfAbsent("interval", k -> 200L));
+        animation.interval(((Number)options.computeIfAbsent("interval", k -> 200L)).longValue());
         return animation;
     }
 }
