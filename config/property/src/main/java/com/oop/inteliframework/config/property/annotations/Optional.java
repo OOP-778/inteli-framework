9c3e58668479cd9aa92d4fe3bc548a6fc1e1229b
oop-778 <oskardhavel@gmail.com>
2021-04-05 15:40:32 +0300

    [config-property] Added Optional annotation

---
+++ config/property/src/main/java/com/oop/inteliframework/config/property/annotations/Optional.java
@@ -0,0 +1,11 @@
+package com.oop.inteliframework.config.property.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface Optional {
+}
