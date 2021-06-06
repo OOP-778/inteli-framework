aa092f810f475da0153d6056a7bdbf2590f25a72
oop-778 <oskardhavel@gmail.com>
2021-06-06 08:28:56 +0300

    Added PropertyHandler#register for cleaner registration

--- config/property/src/main/java/com/oop/inteliframework/config/property/property/custom/PropertyHandler.java
+++ config/property/src/main/java/com/oop/inteliframework/config/property/property/custom/PropertyHandler.java
@@ -1,7 +1,9 @@
 package com.oop.inteliframework.config.property.property.custom;
 
 import com.oop.inteliframework.config.node.api.Node;
+import com.oop.inteliframework.config.property.InteliPropertyModule;
 import com.oop.inteliframework.config.property.property.SerializedProperty;
+import com.oop.inteliframework.plugin.InteliPlatform;
 
 /** Serialize/Deserialize properties */
 public interface PropertyHandler<T> {
@@ -10,4 +12,11 @@ public interface PropertyHandler<T> {
   T fromNode(Node node);
 
   Class<T> getObjectClass();
+
+  default void register() {
+    InteliPlatform
+            .getInstance()
+            .safeModuleByClass(InteliPropertyModule.class)
+            .registerHandler(this);
+  }
 }
